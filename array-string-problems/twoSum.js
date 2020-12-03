5b7019979f71e5c220cba48fb0dd852adfb28395
Taylor Johnson <taylorjohnson141@gmail.com>
2020-12-02 19:55:35 -0700

    Add twoSum

---
+++ array-string-problems/twoSum.js
@@ -0,0 +1,20 @@
+//description 
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+//You can return the answer in any order.
+var twoSum = function(nums, target) {
+    
+  for(let i = 0 ;i<nums.length;i++){
+      for(let j =1;j<nums.length;j++){
+          if(j==i){
+             continue 
+          }else{
+              if(nums[i] + nums[j] === target){
+                  return [i,j]
+              }
+          }
+      }
+  } 
+};
