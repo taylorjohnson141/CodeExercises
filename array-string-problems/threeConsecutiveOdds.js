8a5b81da142845bf2b4a90aeccf66a17559631e8
Taylor Johnson <taylorjohnson141@gmail.com>
2020-12-03 17:14:49 -0700

    Add threeConsecutiveOdd solution

--- array-string-problems/threeConsecutiveOdds.js
+++ array-string-problems/threeConsecutiveOdds.js
@@ -1,5 +1,8 @@
-
-
+// description
+//Given an integer array arr, 
+//return true if there are three consecutive odd numbers in the array. 
+//Otherwise, return false.
+ 
 var threeConsecutiveOdds = function(arr) {
   for(let i = 0 ; i< arr.length; i++){
       console.log('num,arr 1',i,arr[i] % 2 !== 0 )
