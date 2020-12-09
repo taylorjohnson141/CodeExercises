21483076632cd416cee3b15f798b00d8b3e3cc16
Taylor Johnson <taylorjohnson141@gmail.com>
2020-12-08 19:15:30 -0700

    Add majorityElement solution

--- array-string-problems/majorityElement.js
+++ array-string-problems/majorityElement.js
@@ -1,3 +1,9 @@
+//description 
+//Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+//You may assume that the array is non-empty and the majority element always exist in the array.
+
+
 var majorityElement = function(nums) {
   for(let i = 0 ; i<nums.length ; i++ ){
       let currNum = 0
