38ea62d92928c45bb3a07962c086b75f24923b06
Taylor Johnson <taylorjohnson141@gmail.com>
2020-12-12 18:31:52 -0700

    Add checkIfExist solution

--- array-string-problems/findDuplicateNumber.js
+++ array-string-problems/findDuplicateNumber.js
@@ -1,4 +1,4 @@
-// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+// Description :Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
 
 // There is only one duplicate number in nums, return this duplicate number.
 
