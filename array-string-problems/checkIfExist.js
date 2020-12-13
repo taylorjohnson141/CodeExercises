20644046e5ae710363ec75b8d0adbf69dc93560a
Taylor Johnson <taylorjohnson141@gmail.com>
2020-12-12 18:33:09 -0700

    Add check if exist description

--- array-string-problems/checkIfExist.js
+++ array-string-problems/checkIfExist.js
@@ -1,3 +1,9 @@
+//describe 
+//Given an array arr of integers, 
+//check if there exists two integers N and M such 
+//that N is the double of M ( i.e. N = 2 * M).
+
+
 var checkIfExist = function(arr) {
   for(let i = 0 ; i< arr.length; i++){
       for(let j = 0 ; j<arr.length; j++){
