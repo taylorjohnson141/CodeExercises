eef381101f4ea33b783f6933c36efed8e950abb2
Taylor Johnson <taylorjohnson141@gmail.com>
2020-12-15 18:26:21 -0700

    Add containsNearbyDuplicate solution

--- array-string-problems/containsNearbyDuplicate.js
+++ array-string-problems/containsNearbyDuplicate.js
@@ -1,3 +1,7 @@
+//Given an array of integers and an integer k, 
+// find out whether there are two distinct indices i and 
+// j in the array such that nums[i] = nums[j] 
+// and the absolute difference between i and j is at most k.
 var containsNearbyDuplicate = function(nums, k) {
   for(let i =0; i<nums.length; i++){
   for(let j = 0 ; j<nums.length; j++){
