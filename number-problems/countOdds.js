b3ad7392f8c9170364be644d8125377b5fe4795a
Taylor Johnson <taylorjohnson141@gmail.com>
2020-12-16 19:55:34 -0700

    Add CountOdds description

--- number-problems/countOdds.js
+++ number-problems/countOdds.js
@@ -1,4 +1,5 @@
-
+//Given two non-negative integers low and high. 
+//Return the count of odd numbers between low and high (inclusive).
 var countOdds = function(low, high) {
   let odd = 0
   for(let i = low; i<=high;i++){
