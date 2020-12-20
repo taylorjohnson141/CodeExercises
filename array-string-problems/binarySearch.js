8174c85af6c1ee879822c12c1972b712240c5903
Taylor Johnson <taylorjohnson141@gmail.com>
2020-12-19 19:30:10 -0700

    Add binarySearch solutuion

--- array-string-problems/binarySearch.js
+++ array-string-problems/binarySearch.js
@@ -1,3 +1,4 @@
+//Given a sorted (in ascending order) integer array nums of n elements and a target value, write a function to search target in nums. If target exists, then return its index, otherwise return -1.
 
 var search = function(nums, target) {
   return nums.indexOf(target) 
