64acc96a5c99db90e6f8ba89ea717f5a2c7d24d5
Taylor Johnson <taylorjohnson141@gmail.com>
2021-01-31 18:16:16 -0700

    Add peakIndexMountainArray solution

--- array-string-problems/peakIndexInMountainArray.js
+++ array-string-problems/peakIndexInMountainArray.js
@@ -4,3 +4,11 @@
 // arr[0] < arr[1] < ... arr[i-1] < arr[i]
 // arr[i] > arr[i+1] > ... > arr[arr.length - 1]
 // Given an integer array arr that is guaranteed to be a mountain, return any i such that arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+
+var peakIndexInMountainArray = function(arr) {
+  let newArr = [...arr]
+  arr.sort((num1, num2) =>{
+      return num2 - num1
+  })
+  return newArr.indexOf(arr[0])
+};
