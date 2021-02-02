42a9c10169251e698904ba0c2180c250b975ccbe
Taylor Johnson <taylorjohnson141@gmail.com>
2021-02-01 19:58:28 -0700

    Add isAnagram solution

--- array-string-problems/isAnagram.js
+++ array-string-problems/isAnagram.js
@@ -1,3 +1,6 @@
+// Given two strings s and t , write a function to determine if t is an anagram of s.
+
+
 var isAnagram = function(s, t) {
   let sArr = s.split('').sort().join('')
   let tArr = t.split('').sort().join('')
