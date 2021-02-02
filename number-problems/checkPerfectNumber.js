5def1429f46fd862cab4ccd42101fbf0c77494de
Taylor Johnson <taylorjohnson141@gmail.com>
2021-02-01 17:32:16 -0700

    Add perfect number description

--- number-problems/checkPerfectNumber.js
+++ number-problems/checkPerfectNumber.js
@@ -1,3 +1,7 @@
+//A perfect number is a positive integer that is equal to the sum of 
+//its positive divisors, excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
+
+Given an integer n, return true if n is a perfect number, otherwise return false.
 var checkPerfectNumber = function(num) {
   let factorials   = []
   for(let i = 0; i< num; i++){
