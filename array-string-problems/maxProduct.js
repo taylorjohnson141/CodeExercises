2a0c7c0fbb6db5e2a549b5c6f86ee1c5cd401b8c
Taylor Johnson <taylorjohnson141@gmail.com>
2021-02-03 15:30:19 -0700

    Add maxProduct solution

--- array-string-problems/maxProduct.js
+++ array-string-problems/maxProduct.js
@@ -1,3 +1,6 @@
+//Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+
+
 var maxProduct = function(nums) {
   nums.sort((a,b)=>{
       return b -a
