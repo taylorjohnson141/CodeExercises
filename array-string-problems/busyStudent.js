96de7497f35d946cc6396aa34c952c65ab5cc38f
Taylor Johnson <taylorjohnson141@gmail.com>
2021-02-03 14:20:07 -0700

    Add busyStudent description

--- array-string-problems/busyStudent.js
+++ array-string-problems/busyStudent.js
@@ -1,3 +1,7 @@
+// Given two integer arrays startTime and endTime and given an integer queryTime.
+// The ith student started doing their homework at the time startTime[i] and finished it at time endTime[i].
+// Return the number of students doing their homework at time queryTime. More formally, return the number of students where queryTime lays in the interval [startTime[i], endTime[i]] inclusive.
+
 var busyStudent = function(startTime, endTime, queryTime) {
   let combinedArray = []
   for(let i = 0 ;i<startTime.length; i++){
