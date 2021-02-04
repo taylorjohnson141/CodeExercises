40fbf8ccb56f767de34177eab04d370acd85a266
Taylor Johnson <taylorjohnson141@gmail.com>
2021-02-03 18:09:29 -0700

    Add is Palindrome solution

---
+++ linked-list/isPalindrome.js
@@ -0,0 +1,19 @@
+var isPalindrome = function(head) {
+  let arr = []
+  let currhead = head
+  while(currhead !== null){
+      for(let prop in currhead){
+          if(prop === 'val' ){
+              arr.push(currhead[prop])
+          }
+          if(prop ==='next'){
+              currhead = currhead.next                 
+          }   
+      }
+  }
+  if(arr.length ===1){
+      return true
+  }
+  let reverse = [...arr].reverse()
+  return JSON.stringify(reverse) ===JSON.stringify(arr)
+};
