71cc0f63cbb10199a0de26fa47889b807f69d615
Taylor Johnson <taylorjohnson141@gmail.com>
2021-02-27 12:23:26 -0700

    Add FindLuckyNumber description

--- array-string-problems/findLuckyNumber.js
+++ array-string-problems/findLuckyNumber.js
@@ -1,3 +1,6 @@
+// Given an array of integers arr, a lucky integer is an integer which has a frequency in the array equal to its value.
+// Return a lucky integer in the array. If there are multiple lucky integers return the largest of them. If there is no lucky integer return -1.
+
 var findLucky = function(arr) {
   let allFreq = arr.reduce((allFreq, currNum) =>{
       if(allFreq[currNum] === undefined){
